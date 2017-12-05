2784e0ac83f69c804abea53d6effd03e9db00952
Ramon Ferreira <ramon_felipe@outlook.com>
2017-12-05 21:08:03 -0200

    bala esta sendo destruída

---
+++ Assets/ArenaOfGods/Scripts/BulletBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletBehaviour : NetworkBehaviour {
+
+    [Header("Debug")]
+    [SerializeField] private bool _showDebugMessages;
+
+    void OnCollisionEnter()
+    {
+        if (_showDebugMessages) Debug.Log("Iniciando Destruição da Bala...");
+        if (_showDebugMessages) Debug.Log("I'm a Server: " + isServer);
+        if (_showDebugMessages) Debug.Log("I'm a Client: " + isClient);
+
+        NetworkServer.Destroy(this.gameObject);
+    }
+}
